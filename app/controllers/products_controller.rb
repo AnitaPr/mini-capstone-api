cb864f17b9f64841cbb96b3e731244985b5331ad
AnitaPr <anitabllaca88@gmail.com>
2022-03-10 19:50:48 -0500

    Url route and find by method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,16 +1,16 @@
 class ProductsController < ApplicationController
-  def first_product_method
-    product = Product.first
-    render json: product.as_json
-  end
+  # def first_product_method
+  #   product = Product.first
+  #   render json: product.as_json
+  # end
 
   def all_products_method
-    product = Product.all
-    render json: product.as_json
+    watermelon = Product.find_by(name: "Watermelon")
+    render json: watermelon.as_json
   end
 
-  def apple_method
-    product = Product.first
-    render json: product.as_json
-  end
+  # def apple_method
+  #   product = Product.first
+  #   render json: product.as_json
+  # end
 end
